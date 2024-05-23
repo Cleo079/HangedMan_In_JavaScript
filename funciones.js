cb3955b2dd74b97c683586a3bfd3da3c10c57499
Cleo079 <126862358+Cleo079@users.noreply.github.com>
2024-05-23 18:01:16 +0200

    Add files via upload

---
+++ funciones.js
@@ -0,0 +1,13 @@
+function id(str){
+    return document.getElementById(str);
+}
+
+function querySAll(str){
+    return document.querySelectorAll(str);
+}
+function obtener_random(num_min, num_max){
+    const amplitud_valores = num_max - num_min;
+    const valor_al_azar = Math.floor( Math.random( ) * amplitud_valores)
+    + num_min;
+    return valor_al_azar;
+}
